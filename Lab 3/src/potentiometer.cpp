8f9c81b78366b63d7f17b4fa764ec654f76180c7
bxk473 <bxk473@case.edu>
2026-09-13 19:58:07 -0400

    Update potentiometer pin and print sensor values

--- Lab 3/src/potentiometer.cpp
+++ Lab 3/src/potentiometer.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 //Put your potentiometer pin assignment here
-const int sensorPin = 0;
+const int sensorPin = A0;
 
 void setup() {
   Serial.begin(115200);
@@ -9,6 +9,8 @@ void setup() {
 
 void loop() {
   //This line should print out the values from the sensor by Serial.println(??);
-  
+  Serial.println(analogRead(sensorPin));
   delay(50); 
 }
+
+
