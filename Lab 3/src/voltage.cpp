842005a2d75dcac4375f5ed1b279e55c036c86e8
bxk473 <bxk473@case.edu>
2026-09-13 19:56:51 -0400

    Update voltage calculation and sensor pin assignment
    Refactor voltage calculation to use a variable for analog read value and update delay.

--- Lab 3/src/voltage.cpp
+++ Lab 3/src/voltage.cpp
@@ -4,7 +4,7 @@
 int voltage(float analogvalue);
 
 // Put your potentiometer pin assignment here
-const int sensorPin = 0;
+const int sensorPin = A0;
 
 void setup() {
     Serial.begin(115200);
@@ -12,14 +12,18 @@ void setup() {
 
 void loop() {
     // This is the line for printing in the serial
-    Serial.println(voltage(analogRead(sensorPin)));
-    delay(50); 
+    int sensorValue = analogRead(sensorPin); // (bxk473) Made a variable to store the analogRead value for use in voltage calculation
+    float sensorVoltage = (sensorValue * 3.3) / 4095; // (bxk473) Calculate the voltage based on the analogRead value, assuming a 3.3V reference and 12-bits
+    Serial.println(sensorVoltage); // (bxk473) Print the calculated voltage to the serial monitor);
+    delay(250); // (bxk473) Delay for 500 mil liseconds before the next reading
 }
 
 // function to calculate output voltage
 int voltage(float analogvalue){
     int voltage;
+    
     //analogRead(sensorPin); gives us 0-4095 values
     //use the formula (Analog value*Reference voltage) / (Sensor Resolution) to calculate the output voltage
     return voltage;
 }
+
