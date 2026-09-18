bd1dc375398e460477dc4c992fde0ac1c47f3c75
bxk473 <bxk473@case.edu>
2026-09-18 14:35:00 -0400

    Update Servo Motor Random.cpp

--- Lab 4/src/Servo Motor Random.cpp
+++ Lab 4/src/Servo Motor Random.cpp
@@ -4,7 +4,7 @@
 
 // Define the servo and the pin it is connected to, what is your servo pin?
 Servo myServo;
-const int servoPin = 0;
+const int servoPin = A0; // (bxk473) define the servo and assign it to A0 as shown in the wiring diagram
 
 // variable for random angle
 int randomAngle;
@@ -26,11 +26,12 @@ void setup() {
 
 void loop() {
     //  --- SECTION 1: Make a Random Angle Between 0 to 180 ---
-    // randomAngle = ?; // random(A,B); returns a random value between A and B
+    randomAngle = random(-1, 181); // (bxk473) generates a random angle between 0 and 180 degrees
 
     // ---SECTION 2: Map Pulse Width with Angle
-    // pulseWidth = map(?, ?, ?, ?, ?, ?) // from Servo Motor.cpp, what did you learn from using map function?
-    myServo.writeMicroseconds(pulseWidth); // writing pulse width to servo
+    pulseWidth = map(randomAngle, 0, 180, minPulseWidth, maxPulseWidth); // (bxk473) the map function is a function that maps 0 degrees to 500 pulse width, and 180 degrees to 2500 pulse width
+    myServo.writeMicroseconds(pulseWidth); // (bxk473) write the pulse width to the servo to move it to the random angle we generated earlier
 
-    delay(1000); // change delay to your own preference
+    delay(500); // Make the servo wait for 500 milliseconds before generating a new random angle
 }
+
