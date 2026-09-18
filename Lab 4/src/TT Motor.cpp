a2067fd05c85e44efd1f151ada57f4fb1b8daaa6
bxk473 <bxk473@case.edu>
2026-09-18 14:36:23 -0400

    Configure motor pins and extend delay
    Updated motor control pins and adjusted delay in setup.

--- Lab 4/src/TT Motor.cpp
+++ Lab 4/src/TT Motor.cpp
@@ -1,20 +1,20 @@
 #include <Arduino.h>
 
-// TODO: Define your pins
-// Hint: Look at your wiring. Which pins did you use?
-const int MOTOR_B_1A = 0; // Replace 0 with your pin number
-const int MOTOR_B_1B = 0; // Replace 0 with your pin number
+// (bxk473) assign pins A0 and A1 to match the motor pins as shown in the wiring diagram 
+const int MOTOR_B_1A = A1; 
+const int MOTOR_B_1B = A0; 
 
 
 void setup() {
 
+    // (bxk473) I modified the analogWrite, delay, and swapped it all then reset the code to default
   pinMode(MOTOR_B_1A, OUTPUT);
   pinMode(MOTOR_B_1B, OUTPUT);  
 
   analogWrite(MOTOR_B_1A, 255);
   analogWrite(MOTOR_B_1B, 0);
 
-  delay(5000);
+  delay(10000);
 
   analogWrite(MOTOR_B_1A, 0);  
   analogWrite(MOTOR_B_1B, 0);
